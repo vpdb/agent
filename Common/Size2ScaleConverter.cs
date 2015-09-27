b9ae5224ee1605a50d5ad1857cf9d48c727d35a3
freezy <freezy@xbmc.org>
2015-09-27 15:16:51 +0200

    Added close icon.

---
+++ Common/Size2ScaleConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace VpdbAgent.Common
+{
+	public class Size2ScaleConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+
+			var newValue = (double)((double)value / 56.287); // this is converted for assigning the ScaleX and ScaleY
+
+			return newValue;
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			return value;
+		}
+	}
+}
