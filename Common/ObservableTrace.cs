78d614a3061663eec8380288a4089bf99adfecc6
freezy <freezy@xbmc.org>
2015-10-18 12:14:15 +0200

    Moved SmoothPanel to Libs folder.

---
+++ Common/ObservableTrace.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VpdbAgent.Common
+{
+	public static class ObservableTrace
+	{
+		public static IObservable<TSource> Trace<TSource>(this IObservable<TSource> source, string name)
+		{
+			var id = 0;
+			return Observable.Create<TSource>(observer =>
+			{
+				var id1 = ++id;
+				Action<string, object> trace = (m, v) => Debug.WriteLine("-----> {0}{1}: {2}({3})", name, id1, m, v);
+
+				trace("Subscribe", "");
+				var disposable = source.Subscribe(
+					v => { trace("OnNext", v); observer.OnNext(v); },
+					e => { trace("OnError", ""); observer.OnError(e); },
+					() => { trace("OnCompleted", ""); observer.OnCompleted(); });
+				return () => { trace("Dispose", ""); disposable.Dispose(); };
+			});
+		}
+	}
+}
