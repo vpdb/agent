26976e2a764d57ca5739862da3d867ab6f1abb49
freezy <freezy@xbmc.org>
2015-10-22 00:09:36 +0200

    Download stream obtained.

--- Vpdb/DownloadManager.cs
+++ Vpdb/DownloadManager.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Reactive.Concurrency;
 using System.Reactive.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using NLog;
 
 namespace VpdbAgent.Vpdb
@@ -33,9 +36,27 @@ namespace VpdbAgent.Vpdb
 			_logger.Info("Retrieving details for release {0}...", id);
 			_vpdbClient.Api.GetRelease(id)
 				.ObserveOn(Scheduler.Default)
-				.Subscribe(release => {
+				.Subscribe(release =>
+				{
+					// todo make this more sophisticated based on settings
+					var file = release.Versions
+						.SelectMany(v => v.Files)
+						.FirstOrDefault(f => f.Flavor.Orientation.Equals("fs"));
 
-					_logger.Info("Got something: {0}", release);
+					if (file == null) {
+						_logger.Info("Release doesn't seem to have a FS release, aborting.");
+						return;
+					}
+
+					var req = _vpdbClient.GetWebRequest(file.Reference.Url);
+					_logger.Info("Downloading: {0}", req.RequestUri);
+
+					// convert to observable
+					var task = Task<WebResponse>.Factory.FromAsync(req.BeginGetResponse, req.EndGetResponse, req);
+					Observable.FromAsync(ct => task).Subscribe(response => {
+						_logger.Info("Got a stream!");
+					});
+
 				}, error => {
 					_logger.Error(error, "Error retrieving release data.");
 				});
