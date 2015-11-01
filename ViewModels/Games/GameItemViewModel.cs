c4fd16132250ac6475594d7743b823f92be9a182
freezy <freezy@xbmc.org>
2015-11-01 15:34:06 +0100

    Releases now get automatically linked if file name and file size is identical as local file.

--- ViewModels/Games/GameItemViewModel.cs
+++ ViewModels/Games/GameItemViewModel.cs
@@ -6,6 +6,7 @@ using System.Reactive.Linq;
 using NLog;
 using ReactiveUI;
 using Splat;
+using VpdbAgent.Application;
 using VpdbAgent.Vpdb;
 using VpdbAgent.Vpdb.Models;
 using Game = VpdbAgent.Models.Game;
@@ -14,9 +15,12 @@ namespace VpdbAgent.ViewModels.Games
 {
 	public class GameItemViewModel : ReactiveObject
 	{
+		private const long MatchThreshold = 512;
+
 		// deps
 		private static readonly Logger Logger = Locator.CurrentMutable.GetService<Logger>();
 		private static readonly IVpdbClient VpdbClient = Locator.CurrentMutable.GetService<IVpdbClient>();
+		private static readonly IGameManager GameManager = Locator.CurrentMutable.GetService<IGameManager>();
 
 		// commands
 		public ReactiveCommand<List<Release>> IdentifyRelease { get; protected set; }
@@ -48,15 +52,34 @@ namespace VpdbAgent.ViewModels.Games
 			Game = game;
 
 			// release identify
-			IdentifyRelease = ReactiveCommand.CreateAsyncObservable(_ => VpdbClient.Api.GetReleasesBySize(game.FileSize, 1000000).SubscribeOn(Scheduler.Default));
+			IdentifyRelease = ReactiveCommand.CreateAsyncObservable(_ => VpdbClient.Api.GetReleasesBySize(game.FileSize, MatchThreshold).SubscribeOn(Scheduler.Default));
 			IdentifyRelease.Select(releases => releases
 				.Select(release => new {release, release.Versions})
 				.SelectMany(x => x.Versions.Select(version => new {x.release, version, version.Files}))
 				.SelectMany(x => x.Files.Select(file => new GameResultItemViewModel(game, x.release, x.version, file, CloseResults)))
-			).Subscribe(releases => {
+			).Subscribe(x => {
 
-				// todo try direct match if filename AND size matches.
-				IdentifiedReleases = releases;
+				var releases = x as GameResultItemViewModel[] ?? x.ToArray();
+				var numMatches = 0;
+				GameResultItemViewModel match = null;
+
+				// ReSharper disable once LoopCanBePartlyConvertedToQuery
+				foreach (var vm in releases) {
+					if (game.Filename.Equals(vm.File.Reference.Name) && game.FileSize == vm.File.Reference.Bytes) {
+						numMatches++;
+						match = vm;
+					}
+				}
+
+				// if file name and file size are identical, directly match.
+				if (numMatches == 1 && match != null) {
+					GameManager.LinkRelease(match.Game, match.Release, match.File.Reference.Id);
+
+				} else {
+					IdentifiedReleases = releases;
+					HasExecuted = true;
+				}
+
 			});
 
 			// handle errors
@@ -64,11 +87,6 @@ namespace VpdbAgent.ViewModels.Games
 
 			// spinner
 			IdentifyRelease.IsExecuting.ToProperty(this, vm => vm.IsExecuting, out _isExecuting);
-
-			IdentifyRelease.IsExecuting
-				.Skip(1)             // skip initial false value
-				.Where(x => !x)      // then trigger when false again
-				.Subscribe(_ => { HasExecuted = true; });
 
 			IdentifyRelease.Select(r => r.Count > 0).Subscribe(hasResults => { HasResults = hasResults; });
 
