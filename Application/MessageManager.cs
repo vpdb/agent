a39e033cce30af18940ab6bfcbc6c7930163ee7d
freezy <freezy@xbmc.org>
2015-11-21 15:41:35 +0100

    Added empty message manager.

---
+++ Application/MessageManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VpdbAgent.Application
+{
+	public interface IMessageManager
+	{
+		IMessageManager LogError(Exception e, string message);
+	}
+
+	public class MessageManager : IMessageManager
+	{
+		public IMessageManager LogError(Exception e, string message)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ Models/Message.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VpdbAgent.Models
+{
+	public class Message
+	{
+		public MessageType Type;
+	}
+
+	public enum MessageType
+	{
+		Info, Error
+	}
+}
