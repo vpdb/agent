27d6127d4290185fd8dec6a72d8d5e505ecd9ebf
freezy <freezy@xbmc.org>
2015-12-19 23:25:25 +0100

    Added unit tests for parsing systems from PinballX.ini.

--- VpdbAgent.Tests/MenuManager.Test.cs
+++ VpdbAgent.Tests/MenuManager.Test.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Reactive.Subjects;
+﻿using System.IO;
 using System.Text;
 using IniParser;
 using IniParser.Model;
@@ -10,7 +7,6 @@ using NLog;
 using Splat;
 using VpdbAgent.Application;
 using VpdbAgent.PinballX;
-using VpdbAgent.PinballX.Models;
 using VpdbAgent.Tests.Mocks;
 using Xunit;
 
@@ -19,40 +15,102 @@ namespace VpdbAgent.Tests
 	public class MenuManager
 	{
 		[Fact]
-		public void ShouldReadInitialGames()
+		public void ShouldReadInitialSystem()
 		{
-			var pinballXIni = GeneratePinballXIni(new[] {
-				@"[VisualPinball]",
-				@"Enabled = true",
-				@"WorkingPath = C:\Visual Pinball",
-				@"Executable = VPinball.exe",
-				"Parameters = /play - \"[TABLEPATH]\\[TABLEFILE]\"",
-			});
+			// setup
 			var fam = new Mock<IFileAccessManager>();
-			fam.Setup(f => f.ParseIni(It.IsAny<string>())).Returns(pinballXIni);
+			fam.Setup(f => f.ParseIni(It.IsAny<string>())).Returns(GeneratePinballXIni(Ini1));
 
-			var resolver = TestLocator.Generate();
-			var menuManager = new PinballX.MenuManager(
-				resolver.GetService<IFileSystemWatcher>(),
-				resolver.GetService<ISettingsManager>(),
-				fam.Object,
-				resolver.GetService<IThreadManager>(), 
-				resolver.GetService<CrashManager>(),
-				resolver.GetService<Logger>());
+			var menuManager = GetManager(new TestDependencies().Locator, fam.Object);
 
+			// test 
 			menuManager.Initialize();
 
 			Assert.Equal(1, menuManager.Systems.Count);
 		}
 
+		[Fact]
+		public void ShouldReadInitialSystems()
+		{
+			// setup
+			var fam = new Mock<IFileAccessManager>();
+			fam.Setup(f => f.ParseIni(It.IsAny<string>())).Returns(GeneratePinballXIni(Ini3));
 
-		public static IniData GeneratePinballXIni(string[] ini)
+			var menuManager = GetManager(new TestDependencies().Locator, fam.Object);
+
+			// test
+			menuManager.Initialize();
+
+			Assert.Equal(3, menuManager.Systems.Count);
+		}
+
+		[Fact]
+		public void ShouldUpdateSystems()
+		{
+			// setup
+			var fam = new Mock<IFileAccessManager>();
+			fam.Setup(f => f.ParseIni(It.IsAny<string>())).Returns(GeneratePinballXIni(Ini1));
+
+			var deps = new TestDependencies();
+			var menuManager = GetManager(deps.Locator, fam.Object);
+
+			// test
+			menuManager.Initialize();
+
+			// now, pinballx.ini has changed.
+			fam.Setup(f => f.ParseIni(It.IsAny<string>())).Returns(GeneratePinballXIni(Ini3));
+			deps.PinballXIniWatcher.OnNext("path-to-pinballx.ini");
+
+			Assert.Equal(3, menuManager.Systems.Count);
+		}
+
+		private static IMenuManager GetManager(IDependencyResolver resolver, IFileAccessManager fam)
+		{
+			return new PinballX.MenuManager(
+				resolver.GetService<IFileSystemWatcher>(),
+				resolver.GetService<ISettingsManager>(),
+				fam,
+				resolver.GetService<IThreadManager>(),
+				resolver.GetService<CrashManager>(),
+				resolver.GetService<Logger>());
+		}
+
+		private static IniData GeneratePinballXIni(string[] ini)
 		{
 			var byteArray = Encoding.UTF8.GetBytes(string.Join("\n", ini));
 			var stream = new MemoryStream(byteArray);
 			var parser = new FileIniDataParser();
 			return parser.ReadData(new StreamReader(stream));
 		}
+
+		private static readonly string[] Ini1 = {
+			@"[VisualPinball]",
+			@"Enabled = true",
+			@"WorkingPath = C:\Visual Pinball",
+			@"Executable = VPinball.exe",
+			"Parameters = /play - \"[TABLEPATH]\\[TABLEFILE]\"",
+		};
+
+		private static readonly string[] Ini3 = {
+			@"[VisualPinball]",
+			@"Enabled = true",
+			@"WorkingPath = C:\Visual Pinball",
+			@"Executable = VPinball.exe",
+			"Parameters = /play - \"[TABLEPATH]\\[TABLEFILE]\"",
+
+			@"[FuturePinball]",
+			@"Enabled = true",
+			@"WorkingPath = C:\Future Pinball",
+			@"Executable = FuturePinball.exe",
+			"Parameters = /open \"[TABLEPATH]\\[TABLEFILE]\" /play /exit /arcaderender",
+
+			@"[System_1]",
+			@"Name = MAME",
+			@"Enabled = true",
+			@"WorkingPath = C:\Emulators\MAME",
+			@"Executable = mamep64.exe",
+			"Parameters =[TABLEFILE]",
+		};
 	}
 
 }
---
+++ VpdbAgent.Tests/Mocks/TestDependencies.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Reactive.Subjects;
+using Moq;
+using NLog;
+using Splat;
+using VpdbAgent.Application;
+using VpdbAgent.PinballX;
+using VpdbAgent.PinballX.Models;
+
+namespace VpdbAgent.Tests.Mocks
+{
+	public class TestDependencies
+	{
+		public IDependencyResolver Locator => _locator;
+		public Subject<string> PinballXIniWatcher { get; } = new Subject<string>();
+		public Subject<string> DatabaseWatcher { get; } = new Subject<string>();
+		public Subject<string> TableWatcher { get; } = new Subject<string>();
+
+		public Mock<IFileSystemWatcher> FileSystemWatcher;
+
+		private readonly ModernDependencyResolver _locator;
+
+		public TestDependencies()
+		{
+			_locator = new ModernDependencyResolver();
+
+			// Logger
+			_locator.RegisterLazySingleton(LogManager.GetCurrentClassLogger, typeof(Logger));
+
+			// IFileSystemWatcher
+			FileSystemWatcher = new Mock<IFileSystemWatcher>();
+			FileSystemWatcher.Setup(f => f.FileWatcher(It.IsAny<string>())).Returns(PinballXIniWatcher);
+			FileSystemWatcher.Setup(f => f.DatabaseWatcher(It.IsAny<string>(), It.IsAny<IList<PinballXSystem>>())).Returns(DatabaseWatcher);
+			FileSystemWatcher.Setup(f => f.TablesWatcher(It.IsAny<IList<PinballXSystem>>())).Returns(TableWatcher);
+			_locator.RegisterLazySingleton(() => FileSystemWatcher.Object, typeof(IFileSystemWatcher));
+
+			// ISettingsManager
+			var sm = new Mock<ISettingsManager>();
+			sm.Setup(s => s.Settings).Returns(TestConfig.GenerateSettings());
+			_locator.RegisterLazySingleton(() => sm.Object, typeof(ISettingsManager));
+
+			// CrashManager
+			_locator.RegisterLazySingleton(() => new Mock<CrashManager>(LogManager.GetCurrentClassLogger()).Object, typeof(CrashManager));
+
+			// IThreadManager
+			_locator.RegisterLazySingleton(() => new TestThreadManager(), typeof(IThreadManager));
+		}
+	}
+}
