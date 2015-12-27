8f97ae44e2e604fcc107d450b1a9ad2c11a8ab88
freezy <freezy@xbmc.org>
2015-12-27 15:41:52 +0100

    Updated assembly info.

--- VpdbAgent/Properties/AssemblyInfo.cs
+++ VpdbAgent/Properties/AssemblyInfo.cs
@@ -7,12 +7,12 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("VpdbAgent")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("VPDB Agent")]
+[assembly: AssemblyDescription("A Windows desktop application that syncs local data with VPDB.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("VpdbAgent")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCompany("VPDB")]
+[assembly: AssemblyProduct("VPDB Agent")]
+[assembly: AssemblyCopyright("Copyright © 2016 freezy")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.6.1")]
+[assembly: AssemblyFileVersion("0.0.6.1")]
